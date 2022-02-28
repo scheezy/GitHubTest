e8c7b95040ecd0ae2c5bbd0c1c8825e8d92d304f
fabia <fabia@DESKTOP-9SVSR2D>
2022-02-28 10:28:44 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World to the Moon!");
+        }
+    }
+}
