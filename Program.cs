600ca356d29378b471cfc1620f4d29287a7cce04
sheezy <fabian.holz.1@web.de>
2022-02-28 10:38:59 +0100

    Klasse Person hinzugefügt + 2 Methoden

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace GitHubTest
     {
         static void Main(string[] args)
         {
+            Person p = new Person("Fred");
             Console.WriteLine("Hello World to the Moon!");
+            Ausgabe();
+        }
+
+        public static void Ausgabe()
+        {
+            Console.WriteLine("That's true!");
+        }
+
+        public static void ZitatGeber(Person person)
+        {
+            Console.WriteLine($"- {person.Name}");
         }
     }
 }
